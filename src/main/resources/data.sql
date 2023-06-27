03bc418797ce3caa851af253c4856fc3b2ddeb33
Den <no>
2023-06-27 13:21:36 +0300

    added css for table design

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,16 @@
-INSERT INTO shopping (id, data, how_many, name, store) VALUES
-(1,'2004-10-19 10:23:54','100 гр', 'пакет для пакетов','Шестёрочка');
-SELECT nextVal('"shopping_id_seq"');  /* next value sequence or SELECT setval('the_primary_key_sequence', (SELECT MAX(the_primary_key) FROM the_table)+1);*/
-
 INSERT INTO family (id, name) VALUES
     (2, 'Первая семья');
 INSERT INTO family (id, name) VALUES
     (3, 'Вторая семья');
+
+INSERT INTO shopping (id, data, how_many, name, store, family_id) VALUES
+(1,'2004-10-19 10:23:54','100 гр', 'покупка 1, первой семьи','Шестёрочка', 2),
+(2,'2004-10-19 10:23:54','100 гр', 'покупка 2, первой семьи','Шестёрочка', 2),
+(3,'2004-10-19 10:23:54','100 гр', 'пакет для пакетовб 2-й семьи','Шестёрочка', 3);
+
+SELECT nextVal('"shopping_id_seq"');  /* next value sequence or SELECT setval('the_primary_key_sequence', (SELECT MAX(the_primary_key) FROM the_table)+1);*/
+SELECT nextVal('"shopping_id_seq"');  /* next value sequence or SELECT setval('the_primary_key_sequence', (SELECT MAX(the_primary_key) FROM the_table)+1);*/
+SELECT nextVal('"shopping_id_seq"');  /* next value sequence or SELECT setval('the_primary_key_sequence', (SELECT MAX(the_primary_key) FROM the_table)+1);*/
 
 INSERT INTO users (id, name) VALUES
 (5, 'Коля');
